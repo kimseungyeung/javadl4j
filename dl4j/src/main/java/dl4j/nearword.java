1596c838a8ee3760eae936bb346894e3e4e98ad9
HIDAYZ <tests>
2019-05-15 15:58:09 +0900

    xzc

--- dl4j/src/main/java/dl4j/nearword.java
+++ dl4j/src/main/java/dl4j/nearword.java
@@ -7,25 +7,29 @@ import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.word2vec.Word2Vec;
 
 public class nearword {
-public static Word2Vec vec;
+	public static Word2Vec vec;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		File dir = new File("D:" + "/deeplearning/");
-		if(!dir.exists()) {
+		if (!dir.exists()) {
 			dir.mkdirs();
 		}
-		if(vec==null) {
-		vec =WordVectorSerializer.readWord2VecModel("D:" + "/deeplearning/" + "pathToSaveModel.txt");
-	}
-		
-		Scanner scan = new Scanner(System.in); // ¹®ÀÚ ÀÔ·ÂÀ» ÀÎÀÚ·Î Scanner »ı¼º
+		if (vec == null) {
+			vec = WordVectorSerializer.readWord2VecModel("D:" + "/deeplearning/" + "pathToSaveModel.txt");
+		}
 
-	System.out.println("°Ë»ö°ªÀ» ÀÔ·ÂÇÏ¼¼¿ä:");
-	System.out.println(vec.getVocab().elementAtIndex(4).toString()); //¸ğµ¨ ·©Å· Ã£±â
-	String word = scan.nextLine();
-	String result =vec.wordsNearest(word,5).toString();
-	System.out.println(result);
-	nearword.main(null);
+		Scanner scan = new Scanner(System.in); // ë¬¸ì ì…ë ¥ì„ ì¸ìë¡œ Scanner ìƒì„±
+
+		System.out.println("ê²€ìƒ‰ê°’ì„ ì…ë ¥í•˜ì„¸ìš”:");
+		// System.out.println(vec.getVocab().elementAtIndex(4).toString()); //ëª¨ë¸ ë­í‚¹ ì°¾ê¸°
+		String word = scan.nextLine();
+		String result = vec.wordsNearest(word, 5).toString();
+		result=result.replace("[","");
+		result=result.replace("]","");
+		String[]results=result.split(",");
+		System.out.println(result);
+		nearword.main(null);
 	}
 
 }
